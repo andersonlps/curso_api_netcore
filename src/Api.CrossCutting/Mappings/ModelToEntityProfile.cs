d73299e9eb9fe18aafb7cb13836fc14e03fc5e88
Anderson <andersomlps@gmail.com>
2023-03-15 15:44:31 -0300

    Implementando o AutoMapper para criação de DTO's

---
+++ src/Api.CrossCutting/Mappings/ModelToEntityProfile.cs
@@ -0,0 +1,15 @@
+using Api.Domain.Entities;
+using Api.Domain.models;
+using AutoMapper;
+
+namespace Api.CrossCutting.Mappings
+{
+    public class ModelToEntityProfile : Profile
+    {
+        public ModelToEntityProfile()
+        {
+            CreateMap<UserEntity, UserModel>()
+                .ReverseMap();
+        }
+    }
+}
